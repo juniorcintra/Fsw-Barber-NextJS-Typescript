794cedd0115274c4a365b03344c20558d484a5a4
devjuniorcintra <junior.developer@outlook.com.br>
2024-08-05 20:47:40 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
-  return <Button>test</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Junior!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+        <div className="mt-6 flex flex-row items-center gap-2">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src=""
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
