2ed950fab137d42ee24afc7a9c624607d98fa66f
devjuniorcintra <junior.developer@outlook.com.br>
2024-08-05 21:50:14 -0300

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ import BarbershopItem from "./_components/barbershop-item";
 const Home = async () => {
   // chamar meu banco de dados
   const barbershops = await db.barbershop.findMany({});
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: { name: "desc" },
+  });
 
   return (
     <div>
@@ -75,7 +78,26 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   );
 };
