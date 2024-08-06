f522f6144cb8d8b170268fcd1c94c83ceaa3a1d9
devjuniorcintra <junior.developer@outlook.com.br>
2024-08-05 21:01:46 -0300

    feat: add booking item

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image src={""} alt="FSW Barber" height={18} width={120} />
+        <Image src="logo.png" alt="FSW Barber" height={18} width={120} />
         <Button size="icon" variant="outline">
           <MenuIcon />
         </Button>
